2546414c5dbba5c48f5b506edff34b44e78983fa
doolotkeldi <dolotalanov35@gmail.com>
2024-05-04 18:21:11 +0600

    hello koreya

---
+++ src/components/Koreyan/Koreyan.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Kareyan from "../componentss/Korea/Kareyan";
+import HeroNumber from "../componentss/heroNumber/HeroNumber";
+import CarPotion from "../componentss/carPotion/CarPotion";
+
+function Koreyan() {
+  return (
+    <div>
+      <Kareyan />
+      <HeroNumber />
+      <CarPotion />
+    </div>
+  );
+}
+
+export default Koreyan;
